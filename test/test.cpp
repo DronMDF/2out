4dacd3c6f0307944e79117c5e44c452a97c64427
Andrey Valyaev <dron.valyaev@gmail.com>
2017-05-22 12:01:49 +0300

    #1: Make test success

--- test/test.cpp
+++ test/test.cpp
@@ -10,8 +10,8 @@ int main(int, char **)
 		"Example suite",
 		{
 			make_shared<const Case>(
-				"2 * 2 should be equal 5",
-				make_unique<const AssertionEqual>(2 * 2, 5)
+				"2 * 2 should be equal 4",
+				make_unique<const AssertionEqual>(2 * 2, 4)
 			),
 		}
 	).run();
