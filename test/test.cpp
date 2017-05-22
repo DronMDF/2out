f7c518be98b122dfb8beab5140fb9511a4f7f0d0
Andrey Valyaev <dron.valyaev@gmail.com>
2017-05-22 13:51:45 +0300

    #2: Style check

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 Andrey Valyaev <dron.valyaev@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
 #include <memory>
 #include <2out.h>
 
