3eab431010c3258853a617b66e5f88f5d942d5c1
Andrey Valyaev <dron.valyaev@gmail.com>
2017-05-24 15:34:41 +0300

    #13: Move AssertionEqial from 2out.h as IsEqual

---
+++ include/IsEqual.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2017 Andrey Valyaev <dron.valyaev@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#pragma once
+#include "Assertion.h"
+
+namespace oout {
+
+// @todo #13:15min IsEqual is a bad Assertion.
+//  Right Assertion should get in args lazy objects, not int.
+class IsEqual final : public Assertion {
+public:
+	IsEqual(int a, int b);
+	bool valid() const override;
+
+private:
+	int a;
+	int b;
+};
+
+}
