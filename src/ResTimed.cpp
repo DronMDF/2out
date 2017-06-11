879a9bc03805794930be20d9fba0e40b6e8b2083
Andrey Valyaev <dron.valyaev@gmail.com>
2017-06-11 23:14:19 +0300

    #98: Format decorator with time

--- src/ResTimed.cpp
+++ src/ResTimed.cpp
@@ -9,13 +9,42 @@
 using namespace std;
 using namespace oout;
 
-ResTimed::ResTimed(const shared_ptr<const Result> &result, float)
-	: result(result)
+
+class FmtTimed final : public Format {
+public:
+	FmtTimed(const Format *format, float time)
+		: format(format), time(time)
+	{
+	}
+
+	string test(const string &name, bool failure, float) const override
+	{
+		return format->test(name, failure, time);
+	}
+
+	string suite(
+		const string &name,
+		float,
+		const list<shared_ptr<const Result>> &results
+	) const override
+	{
+		return format->suite(name, time, results);
+	}
+
+private:
+	FmtTimed(const FmtTimed&) = delete;
+	FmtTimed &operator =(const FmtTimed&) = delete;
+
+	const Format *format;
+	const float time;
+};
+
+ResTimed::ResTimed(const shared_ptr<const Result> &result, float time)
+	: result(result), time(time)
 {
 }
 
 string ResTimed::print(const Format &format) const
 {
-	// @todo #94:15min Implement Timed Format whoes bind time inside
-	return result->print(format);
+	return result->print(FmtTimed(&format, time));
 }
