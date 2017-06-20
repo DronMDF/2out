2aa0094976cf8c844071d42a81ff8a2253d87a91
Andrey Valyaev <dron.valyaev@gmail.com>
2017-06-20 23:37:24 +0300

    #143: Assertion is a Test, new view.

--- test/JUnitXmlReportTest.cpp
+++ test/JUnitXmlReportTest.cpp
@@ -67,6 +67,59 @@ private:
 	const string text;
 };
 
+// @todo #143:15min StringRepr is very common interface, can be used in other tests.
+//  For code sharing Test classes in primary ctor get StringRepr pointer.
+//  Secondary stor can translate to repr string (special StringRepr).
+//  Other object can be wrapped by StringRepr over ostream...
+class StringRepr {
+public:
+	virtual ~StringRepr() = default;
+	virtual string asString() const = 0;
+};
+
+class ReportAsString final : public StringRepr {
+public:
+	explicit ReportAsString(const shared_ptr<const JUnitXmlReport> &report)
+		: report(report)
+	{
+	}
+
+	string asString() const override {
+		return report->asString();
+	}
+
+private:
+	const shared_ptr<const JUnitXmlReport> report;
+};
+
+// @todo #143:15min Everythings is test. Assertion is not need.
+//  This test can give any input parameters by wrapping it in StringRepr...
+class IsContainText final : public Test {
+public:
+	IsContainText(const shared_ptr<const StringRepr> &object, const string &text)
+		: object(object), text(text)
+	{
+	}
+
+	shared_ptr<const Result> result() const override {
+		shared_ptr<const AssertionResult> assertion_result;
+		try {
+			if (object->asString().find(text) != string::npos) {
+				assertion_result = make_shared<Success>();
+			} else {
+				assertion_result = make_shared<Failure>();
+			}
+		} catch (const exception &e) {
+			assertion_result = make_shared<Error>(e.what());
+		}
+		return make_shared<const ResSimple>("", assertion_result);
+	}
+
+private:
+	const shared_ptr<const StringRepr> object;
+	const string text;
+};
+
 JUnitXmlReportTest::JUnitXmlReportTest()
 : tests(
 	make_unique<const TstSuite>(
@@ -87,6 +140,14 @@ JUnitXmlReportTest::JUnitXmlReportTest()
 					make_unique<ResErrorCase>()
 				),
 				make_unique<IsTextContain>("</error>")
+			),
+			make_shared<IsContainText>(
+				make_shared<ReportAsString>(
+					make_unique<JUnitXmlReport>(
+						make_unique<ResFailureCase>()
+					)
+				),
+				"<failure"
 			)
 		}
 	)
