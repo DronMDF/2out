ae063126cc2329bba13c5447bf5ea8739231bedc
Andrey Valyaev <dron.valyaev@gmail.com>
2017-08-31 22:17:46 +0300

    #256: Turn off uncrustify check (CI not ready yet)

--- style.sh
+++ style.sh
@@ -5,7 +5,10 @@ set -ue
 # Check coding style
 cppcheck -I. -Itest --enable=all --suppress=missingIncludeSystem --quiet 2out test
 ./cpplint.py `find 2out/ -name *.cpp -o -name *.h` `find test/ -name *.cpp -o -name *.h`
-uncrustify -c uncrustify.conf --check -q `find 2out/ -name *.cpp -o -name *.h` `find test/ -name *.cpp -o -name *.h`
+# @todo #256 uncrustify in ubuntu Trusty is very old (0.59)
+#  this version not understood c++11 features and trash my code.
+#  wait for 0.64
+#uncrustify -c uncrustify.conf --check -q `find 2out/ -name *.cpp -o -name *.h` `find test/ -name *.cpp -o -name *.h`
 
 # Check pdd tokens
 pdd > /dev/null
