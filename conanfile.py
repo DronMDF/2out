3c24b2623483f8f5d11b457b4b86340a1edbeb2c
Andrey Valyaev <dron.valyaev@gmail.com>
2018-02-07 22:21:44 +0300

    #284: Cmakefication in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -17,8 +17,8 @@ class OoutConan(ConanFile):
 
 	def build(self):
 		cmake = CMake(self)
-		self.run('cmake 2out %s' % cmake.command_line)
-		self.run("cmake --build . %s" % cmake.build_config)
+		cmake.configure()
+		cmake.build()
 
 	def package(self):
 		self.copy("*.h", dst="include/2out", src="2out")
