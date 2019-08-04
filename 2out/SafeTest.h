2c50a508266d0eab4ca5c286be9a9ca5b12722d2
Andrey Valyaev <dron.valyaev@gmail.com>
2019-08-04 18:36:25 +0300

    #335: Rename TestSafe to SafeTest

---
+++ 2out/SafeTest.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2017-2019 Andrey Valyaev <dron.valyaev@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#pragma once
+#include "Test.h"
+
+namespace oout {
+
+/// Test decorator for catch exceptions, and report error
+class SafeTest final : public Test {
+public:
+	/// Primary ctor
+	explicit SafeTest(const std::shared_ptr<const Test> &test);
+
+	/// Get test result
+	std::unique_ptr<const Result> result() const override;
+
+private:
+	const std::shared_ptr<const Test> test;
+};
+
+}
