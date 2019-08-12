92cbcf77a72420c02669070a8fba80e7ba6933ab
Andrey Valyaev <dron.valyaev@gmail.com>
2019-08-12 23:17:38 +0300

    #317: Coding style about class naming

--- 2out/Test.h
+++ 2out/Test.h
@@ -11,6 +11,9 @@ namespace oout {
 class Result;
 
 /// Abstract test interface
+/// Test is a main build unit for test suite
+/// The concrete Test contains SUT inside.
+/// Each SUT evaluated lazily at time the result method is called.
 class Test {
 public:
 	/// virtual dtor
