61cbeac749200d5a904a93914697d9e8e9c4db9c
Andrey Valyaev <dron.valyaev@gmail.com>
2021-09-04 18:18:08 +0300

    #472: SuiteTestTest inherit from dirty::Test for simplify

--- test/SuiteTestTest.cpp
+++ test/SuiteTestTest.cpp
@@ -11,39 +11,34 @@ using namespace std;
 using namespace oout;
 
 SuiteTestTest::SuiteTestTest()
-	: tests(
-		make_unique<NamedTest>(
-			"SuiteTestTest",
-			make_shared<NamedTest>(
-				"SuiteTest with empty list is always success",
-				make_shared<EqualTest>(
-					make_shared<TestText>(
-						make_unique<SuiteTest>(
-							list<shared_ptr<const Test>>{}
-						)
-					),
-					"success"
-				)
-			),
-			make_shared<NamedTest>(
-				"SuiteTest give tests as variadic args",
-				make_shared<EqualTest>(
-					make_shared<TestText>(
-						make_unique<SuiteTest>(
-							make_shared<EqualTest>("1", "1"),
-							make_shared<EqualTest>("2", "2"),
-							make_shared<EqualTest>("3", "3")
-						)
-					),
-					"success"
-				)
+: dirty::Test(
+	make_unique<NamedTest>(
+		"SuiteTestTest",
+		make_shared<NamedTest>(
+			"SuiteTest with empty list is always success",
+			make_shared<EqualTest>(
+				make_shared<TestText>(
+					make_unique<SuiteTest>(
+						list<shared_ptr<const oout::Test>>{}
+					)
+				),
+				"success"
+			)
+		),
+		make_shared<NamedTest>(
+			"SuiteTest give tests as variadic args",
+			make_shared<EqualTest>(
+				make_shared<TestText>(
+					make_unique<SuiteTest>(
+						make_shared<EqualTest>("1", "1"),
+						make_shared<EqualTest>("2", "2"),
+						make_shared<EqualTest>("3", "3")
+					)
+				),
+				"success"
 			)
 		)
 	)
+)
 {
-}
-
-unique_ptr<const Result> SuiteTestTest::result() const
-{
-	return tests->result();
 }
